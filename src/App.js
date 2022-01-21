242ac4abbcc6f6f6ca768376b18308c075fe647c
arm-inqnatevosyan <inqnatevosyan06@mail.ru>
2022-01-21 17:20:56 +0400

    first

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import About from './Components/About';
 import Contact from './Components/Contact';
 import Home from './Components/Home';
 import Portfolio from './Components/Portfolio';
-import Services from './Components/Services';
+
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
-      <Services />
       <Portfolio />
       <Contact />
     </div>
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -9,6 +9,7 @@ import img5 from "../img/Screenshot_5.png";
 function Portfolio(){
     return (
         <main className="aside2">
+            <div className="fl-dir">
             <div className="title" id="portfolio">
                 <h1>My Latest Projects</h1>
                 <h6>There are many variations of passages of Lorem Ipsum availaibe,</h6>
@@ -16,12 +17,12 @@ function Portfolio(){
             </div>
             <div className="portfolio">
             <div className="child">
-                <div className="img-div">
+               <div className="img-div">
                     <img src={img} className="img"/>
                 </div>
                 <div className="texts">
                     <h2>My Website Shop</h2>
-                    <h6>CLick view My Website</h6>
+                    <a href="https://github.com/arm-inqnatevosyan/Eccomerce-shop-Template"><h6>CLick view My Website</h6></a>
                 </div>
             </div>
             <div className="child">
@@ -30,7 +31,7 @@ function Portfolio(){
                 </div>
                 <div className="texts">
                     <h2>My Website Portfolio</h2>
-                    <h6>CLick view My Website</h6>
+                    <a href="https://github.com/arm-inqnatevosyan/My-Portfolio"><h6>CLick view My Website</h6></a>
                 </div>
             </div>
             <div className="child">
@@ -39,19 +40,19 @@ function Portfolio(){
                 </div>
                 <div className="texts">
                     <h2>My Website Phone</h2>
-                    <h6>CLick view My Website</h6>
+                    <a href="https://github.com/arm-inqnatevosyan/Mobile-Web"><h6>CLick view My Website</h6></a>
                 </div>
             </div>
             </div>
 
-            <div className="portfolio">
+            <div className="portfolio" id="port2">
             <div className="child" id="child">
                 <div className="img-div">
                     <img src={img3} className="img"/>
                 </div>
                 <div className="texts">
                     <h2>My Website PhotoGrapher</h2>
-                    <h6>CLick view My Website</h6>
+                    <a href="https://github.com/arm-inqnatevosyan/Photographer-page"><h6>CLick view My Website</h6></a>
                 </div>
             </div>
             <div className="child">
@@ -60,7 +61,7 @@ function Portfolio(){
                 </div>
                 <div className="texts">
                     <h2>My Website Portfolio</h2>
-                    <h6>CLick view My Website</h6>
+                    <a href="https://github.com/arm-inqnatevosyan/web-site1"><h6>CLick view My Website</h6></a>
                 </div>
             </div>
             <div className="child">
@@ -69,8 +70,9 @@ function Portfolio(){
                 </div>
                 <div className="texts">
                     <h2>My Website Menu</h2>
-                    <h6>CLick view My Website</h6>
+                    <a href="https://github.com/arm-inqnatevosyan/food-app.github.io"><h6>CLick view My Website</h6></a>
                 </div>
+            </div>
             </div>
             </div>
         </main>
